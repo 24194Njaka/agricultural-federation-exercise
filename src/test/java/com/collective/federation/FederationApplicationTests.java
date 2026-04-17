3358931e02fbb51b0815cbfb8aef9c3090dd4806
Njaka <hei.herinjaka@gmail.com>
2026-04-17 16:27:39 +0300

    first commit

---
+++ src/test/java/com/collective/federation/FederationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collective.federation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
