000f8f75977dc4ac0f373e58e09dd5f92c6ca1ef
valisoa01 <valisoatolotriniaina@gmail.com>
2026-04-22 11:31:13 +0300

    add project

---
+++ src/main/java/com/argiculturre/ArgiculturreApplication.java
@@ -0,0 +1,13 @@
+package com.argiculturre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgiculturreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgiculturreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/argiculturre/ArgiculturreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argiculturre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgiculturreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
