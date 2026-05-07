380dbbe174b414eb28725424e212335e49e51847
Njaka <hei.herinjaka@gmail.com>
2026-05-07 11:58:11 +0300

    feat: add attendance

---
+++ src/main/java/com/argiculturre/bonus/dto/MemberDescriptionBonus.java
@@ -0,0 +1,16 @@
+package com.argiculturre.bonus.dto;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberDescriptionBonus {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String occupation;
+}
